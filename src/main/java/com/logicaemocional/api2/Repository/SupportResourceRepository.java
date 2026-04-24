b13ade1173dfa084e825e44801409df7d2e2bd87
JuanRocha22 <rochabermudezjuanfelipe6@gmail.com>
2026-04-24 15:39:13 -0500

    merge: resolve develop into emotional journal

--- src/main/java/com/logicaemocional/api2/Repository/SupportResourceRepository.java
+++ src/main/java/com/logicaemocional/api2/Repository/SupportResourceRepository.java
@@ -1,4 +1,4 @@
-package com.logicaemocional.api.Repository;
+package com.logicaemocional.api2.Repository;
 
 import com.logicaemocional.api.Entity.SupportResource;
 import org.springframework.data.jpa.repository.JpaRepository;
