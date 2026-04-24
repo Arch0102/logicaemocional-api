78100331b423f730c1e90646e33f72b037b1af89
Santiago Ortiz <151885267+Arch0102@users.noreply.github.com>
2026-04-24 14:48:10 -0500

    chore: initial Spring Boot setup for logicaemocional api2

---
+++ src/main/java/com/logicaemocional/api2/LogicaemocionalApi2Application.java
@@ -0,0 +1,13 @@
+package com.logicaemocional.api2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicaemocionalApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogicaemocionalApi2Application.class, args);
+    }
+
+}
